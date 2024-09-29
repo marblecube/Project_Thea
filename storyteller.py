c294406fe5e29fe28d2ec37fdfd378316bce9ac6
Vidal Palacios <vidal.palacios@protonmail.com>
2024-09-29 15:16:16 -0700

    updated .env_example and associated os.getenv calls in storyteller.py

--- storyteller.py
+++ storyteller.py
@@ -10,17 +10,17 @@ from openai import OpenAI
 
 # Configuration
 load_dotenv(override=True)
-api_key = os.getenv('ELEVENLABS_API_KEY')
+elevenlabs_api_key = os.getenv('ELEVENLABS_API_KEY')
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
 if not api_key or not openai_api_key:
     raise ValueError("ELEVENLABS_API_KEY or OPENAI_API_KEY environment variable not set. Did you remember to add them in the .env file?")
 
-client = ElevenLabs(api_key=api_key)
+client = ElevenLabs(api_key=elevenlabs_api_key)
 openai_client = OpenAI()
 
 initial_system_message = os.getenv('INITIAL_SYSTEM_MESSAGE')
-elevenlabs_voice = "FVQMzxJGPUBtfz1Azdoy"  # Change this to the desired voice ID
+elevenlabs_voice = os.getenv('ELEVENLABS_VOICE')
 
 def format_history(msg: str, history: list[list[str, str]]):
     chat_history = [{"role": "system", "content": initial_system_message}]
